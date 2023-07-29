e1fec965abbd2b1b9a29a960a9475ec17983bdb9
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2023-07-30 00:13:09 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	const title = 'Serenity Alang-Alang and Bamboo';
+	const description =
+		'We are a business leading in the construction of various bamboo structure, housing, and custom bamboo design made to order';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+</svelte:head>
+
+Hello world
