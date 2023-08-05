d75439fbbb4e1566c2d26a826b279defa6479c57
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2023-08-05 21:10:28 +0800

    InLang installation

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,8 +1,18 @@
 <script>
+	import { i, languages, switchLanguage, language } from "@inlang/sdk-js"
+
 	const title = 'Serenity Alang-Alang and Bamboo';
 	const description =
 		'We are a business leading in the construction of various bamboo structure, housing, and custom bamboo design made to order';
 </script>
+
+
+{#each languages as lang}
+	<button on:click={() =>switchLanguage(lang)}>{lang}</button>
+{/each}
+
+<h1>{i("welcome")}</h1>
+<p>{language}</p>
 
 <svelte:head>
 	<title>{title}</title>
